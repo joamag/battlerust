86fe5c640a888c6f655569b813e3c63148f64598
João Magalhães <joamag@gmail.com>
2024-08-30 18:22:32 +0100

    chore: new wasm loading

--- frontends/web/index.ts
+++ frontends/web/index.ts
@@ -1,5 +1,20 @@
-import { Battleship, size, default as wasm } from "./lib/battlerust";
+import { default as _wasm, Battleship, size } from "./lib/battlerust";
 import { startApp } from "./react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const require: any;
+
+const wasm = async () => {
+    try {
+        await _wasm({ module_or_path: require("./lib/battlerust_bg.wasm") });
+    } catch (err) {
+        if (err instanceof TypeError) {
+            await _wasm();
+        } else {
+            throw err;
+        }
+    }
+};
 
 (async () => {
     await wasm();
